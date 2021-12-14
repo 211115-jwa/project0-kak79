7d0dd74d8e9c6c4810a9e2120f94473ddcea3982
Kimberly Kohel-Hayes <kohelka@gmail.com>
2021-12-14 11:36:39 -0600

    added the code to the method selectOneById

--- src/test/java/com/revature/data/TestUtils.java
+++ src/test/java/com/revature/data/TestUtils.java
@@ -211,7 +211,70 @@ public class TestUtils {
 		return dogList;
 	}
 	
+	public Dog selectOneById(int dogId) {
+		String sql = "SELECT"
+				+ "	dog.id,"
+				+ "	dog.n_me AS dog_name,"
+				+ "	dog.gender,"
+				+ "	breed.breed_name AS breed,"
+				+ "	dog.akc_reg,"
+				+ "	dog.ag_ AS dog_age,"
+				+ "	dog.fixed,"
+				+ "	breed.breed_size,"
+				+ "	breed.breed_group,"
+				+ "	breed.active_lvl,"
+				+ "	breed.bark_amt,"
+				+ "	breed.coat_tp,"
+				+ "	breed.coat_lngth,"
+				+ "	breed.shedd,"
+				+ "	breed.train"
+				+ "	FROM dog"
+				+ "	FULL OUTER JOIN breed"
+				+ " ON breed.id = dog.breed_id"
+				+ " WHERE dog.id = ?;";
+				
+		Dog test = new Dog();
+		
+		try (Connection con = cu.getConnection()) 
+		{
+			PreparedStatement ps = con.prepareStatement(sql);
 
+			ps.setInt(1, dogId);
+
+			ResultSet rs = ps.executeQuery();
+
+			while (rs.next()) 
+			{
+				int id = rs.getInt("id");
+				String name = rs.getString("dog_name");
+				String gender = rs.getString("gender");
+				boolean akcRegistration = rs.getBoolean("akc_reg");
+				int age = rs.getInt("dog_age");
+				boolean fixed = rs.getBoolean("fixed");
+				String brd_name = rs.getString("breed");
+				String size = rs.getString("breed_size");
+				String group = rs.getString("breed_group");
+				String activityLvl = rs.getString("active_lvl");
+				String barkingAmt = rs.getString("bark_amt");
+				String coatType = rs.getString("coat_tp");
+				String coatLength = rs.getString("coat_lngth");
+				String shedding = rs.getString("shedd");
+				String trainability = rs.getString("train");
+				
+				Breed breed = new Breed(brd_name, size, group, activityLvl, 
+					barkingAmt, coatType, coatLength, shedding, trainability);
+				test = new Dog(id, name, gender, akcRegistration,
+							 age, fixed, breed);
+				
+			}
+		} 
+		catch (SQLException d) 
+		{
+			d.printStackTrace();
+		}
+		
+		return test;
+	}
 	
 	
 	
